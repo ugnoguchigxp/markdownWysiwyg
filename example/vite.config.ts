9305e1d40eb2af909bca5671b6988624b8bbdb23
yuji noguchi <y.noguchi@gxp.co.jp>
2025-12-04 22:49:44 +0900

    fix: configure example app to use local source

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      'markdown-wysiwyg-editor/style.css': path.resolve(__dirname, '../src/index.css'),
+      'markdown-wysiwyg-editor': path.resolve(__dirname, '../src/index.ts'),
+    },
+  },
 })
